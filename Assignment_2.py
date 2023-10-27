f61433730f3b1e0b349bddab407093bc428d98a1
Kirill Mukhin <m.kirya05@gmail.com>
2023-10-26 22:23:16 -0400

    This shoul be all, working in the report

--- Assignment_2.py
+++ Assignment_2.py
@@ -5,14 +5,14 @@ pr_code = int(input("Please enter the Product Code: "))
 pr_name = input("Please enter the Profuct Name: ")
 cr_stock = int(input("Please enter the Currect Stock: "))
 pr_price = float(input("Please enter the Product Sale Price: "))
-pr_manifacture_cost = float(input("Please enter the Produc Manifacture Cost: "))
+pr_manufacture_cost = float(input("Please enter the Produc Manufacture Cost: "))
 montly_production = int(input("Please enter estemated monthly production: "))
 print("")
 print("*******Programming Principles Sample Stock Statement*******\n")
 print("Product Code:", pr_code)
 print("Product Name:", pr_name, "\n")
 print("Sale Price:", pr_price, "CAD")
-print("Manifacture Cost:", pr_manifacture_cost, "CAD")
+print("Manufacture Cost:", pr_manufacture_cost, "CAD")
 print("Monthly production:", montly_production, "units (approx.)")
 
 month = 1
@@ -29,4 +29,4 @@ while month != 12:
     month += 1
 
 if month == 12:
-    print("Net Profit:", (total_sold * pr_price) - (total_manif * pr_manifacture_cost), "CAD")
+    print("Net Profit:", (total_sold * pr_price) - (total_manif * pr_manufacture_cost), "CAD")
